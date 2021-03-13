e8cf795fe9bb50b8f79e17688a6aab4984e56b89
2218675712 <2218675712@qq.com>
2021-03-13 09:26:36 +0800

    前台代码拼接案例
    Signed-off-by: 2218675712 <2218675712@qq.com>

---
+++ WebApplication5.Web/aaaaa.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Web.UI;
+
+namespace WebApplication5
+{
+    public partial class aaaaa : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
